0cc6d3158f03d7fc8291930613bfd438c8dbbf93
dizzgdog-rgb <fox29582@yahoo.com.tw>
2024-07-29 16:56:21 +0800

    index commit

---
+++ main.js
@@ -0,0 +1,30 @@
+import './assets/scss/all.scss';
+import './assets/bootstrap-5.1.3/dist/js/bootstrap'
+
+console.log("Hello world!");
+
+// Dynamic Navigation Highlighting
+document.addEventListener('DOMContentLoaded', function () {
+    // 獲取當前頁面的文件名
+    var currentPage = window.location.pathname.split('/').pop().split('.')[0];
+
+    // 找到所有導航項目
+    var navItems = document.querySelectorAll('.nav-item');
+
+    // 遍歷導航項目並設置活動狀態
+    navItems.forEach(function (item) {
+        if (item.getAttribute('data-page') === currentPage) {
+            item.classList.add('active');
+        }
+    });
+});
+
+
+
+// BS5 modal
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+    myInput.focus()
+})
